7ca4322adbaec507be66bd03eb556f486abe7a3e
Ciprian Dorin Craciun <ciprian@volution.ro>
2021-02-08 11:04:37 +0200

    [misc]  Minor updates.

--- sources/main.rs
+++ sources/main.rs
@@ -8,13 +8,8 @@ use crate::prelude::*;
 pub fn main () -> ServerResult {
 	
 	let _routes = Routes::new ();
-	
 	let _handler = Handler::new (_routes);
 	
-	let _configuration = hss::Configuration::localhost_http ()
-			.with_handler_dyn (_handler)
-			.build () ?;
-	
-	return hss::Server::run_and_wait (_configuration);
+	return hss::main_with_handler_dyn (_handler);
 }
 
