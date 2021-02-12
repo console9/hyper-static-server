4bb961e0fe444531e2827957acbb337e7a54a4cc
Ciprian Dorin Craciun <ciprian@volution.ro>
2021-02-12 14:24:40 +0200

    [main]  Add support for specifying a configuration template.

--- sources/main.rs
+++ sources/main.rs
@@ -5,11 +5,11 @@ use crate::hss;
 
 
 
-pub fn main_with_static (_routes : impl Into<hss::Routes>) -> hss::ServerResult {
+pub fn main_with_static (_routes : impl Into<hss::Routes>, _configuration : Option<hss::Configuration>) -> hss::ServerResult {
 	
 	let _routes = _routes.into ();
 	let _handler = crate::server::StaticHandler::new (_routes);
 	
-	return hss::main_with_handler_dyn (_handler);
+	return hss::main_with_handler_dyn (_handler, _configuration);
 }
 
