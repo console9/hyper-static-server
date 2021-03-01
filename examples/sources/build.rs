19b54072a5dbd7346acba0e9c830620da773349c
Ciprian Dorin Craciun <ciprian@volution.ro>
2021-03-01 15:01:57 +0200

    [examples]  Minor update.

--- examples/sources/build.rs
+++ examples/sources/build.rs
@@ -2,17 +2,8 @@
 #![ no_implicit_prelude ]
 
 
-use ::hyper_static_server as hss;
-
-
 fn main () -> () {
 	
-	let mut _builder = hss::Builder::new_with_defaults ();
-	
-	hss::builder_macros! (_builder);
-	
-	::std::include! ("./routes.in");
-	
-	_builder.generate ();
+	::hyper_static_server::build_with_defaults! ("./routes.in");
 }
 
