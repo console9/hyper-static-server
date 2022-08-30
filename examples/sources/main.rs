a7e53776f8c76f328e32c4b1a6d1755730902777
Ciprian Dorin Craciun <ciprian@volution.ro>
2022-08-30 19:33:45 +0300

    [examples]  Organize generated and hand-written code into modules.

--- examples/sources/main.rs
+++ examples/sources/main.rs
@@ -2,34 +2,54 @@
 #![ no_implicit_prelude ]
 
 
-::hyper_static_server::builder_generated! ();
 
 
 pub fn main () -> ::hyper_static_server::errors::MainResult {
 	
 	use ::hyper_static_server::errors::ResultExtWrap as _;
 	
-	let _routes = Routes::new_with_defaults () .else_wrap (0x6836acf2) ?;
+	let _routes = crate::generated::Routes::new_with_defaults () .else_wrap (0x6836acf2) ?;
 	
 	return ::hyper_static_server::main (_routes);
 }
 
 
-#[ derive (::std::fmt::Debug) ]
-#[ derive (::serde::Deserialize) ]
-#[ allow (dead_code) ]
-pub struct ExampleAskamaContext {
-	pub some_map : ::std::collections::HashMap<::std::string::String, ::std::string::String>,
+
+
+pub mod model {
+	
+	
+	#[ derive (::std::fmt::Debug) ]
+	#[ derive (::serde::Deserialize) ]
+	#[ allow (dead_code) ]
+	pub struct ExampleAskamaContext {
+		pub some_map : ::std::collections::HashMap<::std::string::String, ::std::string::String>,
+	}
+	
+	impl ::hyper_static_server::AskamaContextSerde for ExampleAskamaContext {}
+	
+	
+	#[ allow (dead_code) ]
+	pub trait ExampleAskamaTrait : ::hyper_static_server::AskamaTrait<Context = ()> {
+		fn some_fn (&self) -> ::std::string::String {
+			use ::std::convert::From as _;
+			::std::string::String::from ("hello")
+		}
+	}
 }
 
-impl ::hyper_static_server::AskamaContextSerde for ExampleAskamaContext {}
 
 
-#[ allow (dead_code) ]
-pub trait ExampleAskamaTrait : ::hyper_static_server::AskamaTrait<Context = ()> {
-	fn some_fn (&self) -> ::std::string::String {
-		use ::std::convert::From as _;
-		::std::string::String::from ("hello")
-	}
+
+pub mod generated {
+	
+	
+	use crate::model::{
+			ExampleAskamaContext,
+			ExampleAskamaTrait,
+		};
+	
+	
+	::hyper_static_server::builder_generated! ();
 }
 
